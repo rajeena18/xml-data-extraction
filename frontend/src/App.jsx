40b2a605a02498e5d9439fc3df21666d73f0db84
Rahul Jeena <rajeena@alletec.com>
2025-05-26 11:36:10 +0530

    Initial commit - fresh start

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import DataConverter from './components/DataConverter/DataConverter'
+import GetAccessToken from './auth/authTokenAPI'
+import DataConverter2 from './components/DataConverter2/DataConverter2'
+import ResourceDataConverter from './components/DataConverter2/ResourceDataConverter'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        {/* <DataConverter/> */}
+        {/* <GetAccessToken/> */}
+        {/* <DataConverter2/> */}
+        <ResourceDataConverter/>
+      </div>
+    </>
+  )
+}
+
+export default App
